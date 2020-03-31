0e497d10f3b0b251c21fa5d6f76c5a9c639bb086
Taylor Keller <takeller17@gmail.com>
2020-03-31 09:30:41 -0600

    Created test to check that I am adding units to the building

--- test/building_test.rb
+++ test/building_test.rb
@@ -8,10 +8,22 @@ class BuildingTest < Minitest::Test
   def setup
     @building = Building.new
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     @renter1 = Renter.new("Jessie")
   end
 
   def test_it_exists
-    assert_instance_of Building, @building 
+    assert_instance_of Building, @building
+  end
+
+  def test_it_has_no_units_by_default
+    assert_equal [], @building.units
+  end
+
+  def test_add_unit
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+
+    assert_equal [@unit1, @unit2], @building.units 
   end
 end
