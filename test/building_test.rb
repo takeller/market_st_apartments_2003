c8d07a86017a85ecf35884861a2fa558c9809420
Taylor Keller <takeller17@gmail.com>
2020-03-31 09:25:57 -0600

    Added building class file, and building_test file

---
+++ test/building_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter.rb'
+require './lib/apartment.rb'
+
+class BuildingTest < Minitest::Test
+
+  def setup
+    @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @renter1 = Renter.new("Jessie")
+  end
+end
