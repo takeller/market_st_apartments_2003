3ece4a6115e935ac4fcdc3256c2643ab89b95ab7
Taylor Keller <takeller17@gmail.com>
2020-03-31 09:18:15 -0600

    Added attr_reader to apartment class to be able to read it's attributes

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,5 +1,6 @@
 class Apartment
 
+  attr_reader :number, :monthly_rent, :bedrooms, :bathrooms
   def initialize(apartment_details)
     @number = apartment_details[:number]
     @monthly_rent = apartment_details[:monthly_rent]
