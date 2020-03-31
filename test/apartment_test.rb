e6f47a449cc1117255979b310b58d51391fa0841
Taylor Keller <takeller17@gmail.com>
2020-03-31 09:21:05 -0600

    Added test to assert the renter attribute is nil by default, and that we can add a renter

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -7,6 +7,7 @@ class ApartmentTest < Minitest::Test
 
   def setup
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @renter1 = Renter.new("Jessie")
   end
 
   def test_it_exists
@@ -17,6 +18,12 @@ class ApartmentTest < Minitest::Test
     assert_equal "A1", @unit1.number
     assert_equal 1200, @unit1.monthly_rent
     assert_equal 1, @unit1.bedrooms
-    assert_equal 1, @unit1.bathrooms 
+    assert_equal 1, @unit1.bathrooms
+    assert_equal nil, @unit1.renter
+  end
+
+  def test_add_renter
+    @unit1.add_renter(@renter1)
+    assert_equal @renter1, @unit1.renter
   end
 end
