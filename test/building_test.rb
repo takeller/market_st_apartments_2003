4b59e82a089f23a06c298d97e28d7431126488d2
Taylor Keller <takeller17@gmail.com>
2020-03-31 09:40:01 -0600

    Added test that the building has renters when the units have renters

--- test/building_test.rb
+++ test/building_test.rb
@@ -10,7 +10,8 @@ class BuildingTest < Minitest::Test
     @building = Building.new
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-    @renter1 = Renter.new("Jessie")
+    @renter1 = Renter.new("Aurora")
+    @renter2 = Renter.new("Tim")
   end
 
   def test_it_exists
@@ -30,5 +31,15 @@ class BuildingTest < Minitest::Test
     @building.add_unit(@unit2)
 
     assert_equal [@unit1, @unit2], @building.units
+  end
+
+  def test_it_has_renters_if_units_have_a_renter
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+
+    @unit1.add_renter(@renter1)
+    @unit2.add_renter(@renter2)
+
+    assert_equal ["Aurora", "Tim"], @building.renters
   end
 end
